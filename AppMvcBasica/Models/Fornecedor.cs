d423e04560d829de661a426b5f538788c70c6bc1
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-14 20:43:05 -0300

    Modelando a base do banco

---
+++ AppMvcBasica/Models/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EntityFramework Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
