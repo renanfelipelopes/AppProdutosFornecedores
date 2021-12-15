a0dea186fefb1b3550d836b896ad42423536f52c
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-15 07:55:36 -0300

    Criacao das tabelas pelo Migrations

--- AppMvcBasica/Data/Migrations/20211215104922_TabelasSistema.Designer.cs
+++ AppMvcBasica/Data/Migrations/20211215104922_TabelasSistema.Designer.cs
@@ -4,14 +4,16 @@ using AppMvcBasica.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppMvcBasica.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211215104922_TabelasSistema")]
+    partial class TabelasSistema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
