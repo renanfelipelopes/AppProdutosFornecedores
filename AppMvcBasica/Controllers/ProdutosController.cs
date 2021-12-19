890545579bd8ee316c6f920cc44b1984bfdfa0d4
renanflow <renanlopes.ads@gmail.com>
2021-12-18 23:56:48 -0200

    comment

--- AppMvcBasica/Controllers/ProdutosController.cs
+++ AppMvcBasica/Controllers/ProdutosController.cs
@@ -87,7 +87,6 @@ namespace AppMvcBasica.Controllers
         }
 
         // POST: Produtos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
